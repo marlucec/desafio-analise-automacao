d9a6d3723fafda88ba5b0378bdcf5e2ff4671b01
home <marlucecancio@gmail.com>
2021-04-25 23:32:46 -0300

    inserção do teste de login inválido

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -12,5 +12,23 @@ Quando("tentar logar com usuário {string} e inserir a senha {string} válida") 
 end
 
 Então("o sistema deve permitir a autenticação do usuário") do
+end
 
+
+Quando("tentar logar com usuário {string} inválido") do |string|
+  within("form[@id='login_form']") do
+    fill_in 'Login', :with => 'user@example.com'
+  end
+
+end
+
+Quando("Senha {string} inválida") do |string|
+  within("form[@id='login_form']") do
+    fill_in 'Password', :with => 'password'
+  end
+  click_link 'Sign in'
+end
+
+Então("o acesso será negado") do
+  pending # Write code here that turns the phrase above into concrete actions
 end
